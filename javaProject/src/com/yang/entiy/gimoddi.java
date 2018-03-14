ceedbb0401b4124e6f3335297addcd21a6512ef1
nicorn <nicorn1630@naver.com>
2018-03-14 15:25:34 +0900

    ver1

---
+++ javaProject/src/com/yang/entiy/gimoddi.java
@@ -0,0 +1,7 @@
+package com.yang.entiy;
+
+public class gimoddi {
+	public static void main(String[] args) {
+		System.out.println("¾Ó³É");
+	}
+}
---
+++ javaProject/src/com/yang/main/Main.java
@@ -0,0 +1,5 @@
+package com.yang.main;
+
+public class Main {
+
+}
